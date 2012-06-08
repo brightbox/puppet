0abb90e10a4ebfbdf654fba6ae914b098ba88e90
John Leach <john@brightbox.co.uk>
2012-06-08 23:33:17 +0100

    Initial import of apt module

---
+++ modules/apt/lib/puppet/parser/functions/ppa_filename.rb
@@ -0,0 +1,12 @@
+module Puppet::Parser::Functions
+  newfunction(:ppa_filename, :type => :rvalue) do |args|
+    # ppa:brightbox/passenger becomes brightbox-passenger-lucid.list
+    filename = args.first.to_s
+    raise Puppet::ParseError, "ppa_filename cannot be blank" if filename.empty?
+    filename.gsub!(/ppa\:/,'')
+    filename.gsub!('/', '-')
+    filename += "-#{lookupvar('lsbdistcodename')}"
+    filename += ".list"
+    "/etc/apt/sources.list.d/" + filename
+  end
+end
---
+++ modules/apt/manifests/init.pp
@@ -0,0 +1,74 @@
+class apt {
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update",
+    schedule => daily,
+    refreshonly => true
+  }
+
+  # Ensure apt is setup before running apt-get update
+  Apt::Ppa <| |> -> Exec["apt-update"]
+  Apt::Key <| |> -> Exec["apt-update"]
+  Apt::Source <| |> -> Exec["apt-update"]
+
+  # Ensure apt-get update has been run before installing any packages
+  Exec["apt-update"] -> Package <| |>
+}
+
+# $name is arbitrary. attribute "ppa" should be the ppa name, such as
+# "brightbox/ruby-ng" (this is due to a bug in puppet with resources
+# with slashes in them)
+define apt::ppa($ppa = "") {
+  exec { "apt-add-repository-$ppa":
+    command => "/usr/bin/apt-add-repository ppa:$ppa",
+    creates => ppa_filename($ppa),
+    notify => Exec["apt-update"],
+  }
+}
+
+# id should be the 8 character hex string that represents the key (is
+# actually the last 8 characters of the fingeprint)
+define apt::key($id) {
+  exec { "apt-key-$name":
+    command => "/usr/bin/apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys $id",
+    unless => "/usr/bin/apt-key list | /bin/grep $id",
+    notify => Exec["apt-update"]
+  }
+}
+
+# name should be a nice key, like percona, or brightbox
+# source should be: "deb url $lsbdistcodename main"
+# gpg_key_id is a gpg key to import, can be left empty
+define apt::source($source = "", $gpg_key_id = "") {
+  file { "/etc/apt/sources.list.d/${name}.list":
+    content => "# This file is managed by Puppet\n$source",
+    notify => Exec["apt-update"]
+  }
+
+  if $gpg_key_id {
+    # Allow multiple definitions of same key - it's not a problem and
+    # could be a common ocurrence
+    apt::key { "${name}${gpg_key_id}": id => $gpg_key_id }
+    Apt::Key["${name}${gpg_key_id}"] -> File["/etc/apt/sources.list.d/${name}.list"]
+  }
+}
+
+# Adapted from https://github.com/camptocamp/puppet-apt primarily to
+# support their postgresql recpie.
+define apt::preferences($ensure="present", $package="", $pin, $priority) {
+  $pkg = $package ? {
+    "" => $name,
+    default => $package,
+  }
+
+  $fname = regsubst($name, '\.', '-', 'G')
+
+  file {"/etc/apt/preferences.d/$fname":
+    ensure  => $ensure,
+    owner   => root,
+    group   => root,
+    mode    => 644,
+    content => template("apt/preferences.erb"),
+    before  => Exec["apt-update"],
+    notify  => Exec["apt-update"],
+  }
+}
