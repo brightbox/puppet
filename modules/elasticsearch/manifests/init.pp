0b61bf7b315311766ffa2242ababe0445c1445f4
John Leach <john@brightbox.co.uk>
2012-06-20 11:04:26 +0100

    initial elasticsearch module

---
+++ modules/elasticsearch/manifests/init.pp
@@ -0,0 +1,59 @@
+# discovery_hosts should be a an array of hostnames or IPs of other masters
+#
+# Will automatically download debs for versions >= 0.19.  For older
+# versions, you must provide a url to the appropriate deb file using
+# the deburl parameter (you still need to provide the right $version
+# parameter too).
+#
+# To run a client-only server, set $master and $data to false
+#
+# On Lucid it currently requires a manual step to install the package
+# as Lucid apt-get doesn't support trusted=yes parameter
+#
+class elasticsearch($version = "0.19.3", $heap_size = "1024m", $cluster_name = 'elasticsearch', $discovery_hosts = [], $minimum_master_nodes = 1, $deburl = false, $master = true, $data = true) {
+  package { "default-jre-headless":
+    ensure => installed
+  }
+  apt::localpackage { "elasticsearch":
+    url => $deburl ? {
+      false => "https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-${version}.deb",
+      default => $deburl
+    }
+  }
+  package { "elasticsearch":
+    ensure => $version
+  }
+  service { "elasticsearch":
+    ensure => true,
+    enable => true,
+    require => Package[elasticsearch],
+    hasstatus => true,
+    hasrestart => true
+  }
+  file { "/etc/default/elasticsearch":
+    content => template("elasticsearch/default-elasticsearch.erb"),
+    notify => Service[elasticsearch]
+  }
+  file { "/etc/elasticsearch":
+    ensure => directory,
+    require => Package[elasticsearch]
+  }
+  file { "/etc/elasticsearch/elasticsearch.yml":
+    content => template("elasticsearch/elasticsearch.yml.erb"),
+    notify => Service[elasticsearch],
+    require => [Package[elasticsearch], File["/etc/elasticsearch"]]
+  }
+  file { "/etc/elasticsearch/logging.yml":
+    content => template("elasticsearch/logging.yml.erb"),
+    notify => Service[elasticsearch],
+    require => [Package[elasticsearch], File["/etc/elasticsearch"]]
+  }
+}
+
+class elasticsearch::apacheproxy($server_name = "elasticsearch", $htpasswd_filename = "/etc/apache2/elasticsearch.htpasswd") {
+  apache::site { "elasticsearch":
+    content => template("elasticsearch/elasticsearch-apache.conf.erb")
+  }
+  apache::module { 'proxy': conf => false }
+  apache::module { 'proxy_http': conf => false }
+}
---
+++ modules/elasticsearch/spec/classes/elasticsearch_spec.rb
@@ -0,0 +1,31 @@
+require File.join(File.dirname(__FILE__), '../spec_helper')
+
+describe 'elasticsearch', :type => :class do
+  describe "with a version" do
+    let(:params) { { :version => "0.20.2" } }
+    it { should contain_package('elasticsearch').
+       with_ensure('0.20.2')
+    }
+    it { should contain_apt__localpackage("elasticsearch").
+       with_url(/elasticsearch-0.20.2.deb$/)
+    }
+  end
+
+  describe "with a version and a deburl" do
+    let(:params) { { :version => "0.30.3", :deburl => "https://somehost/elasticsearch.deb" } }
+    it { should contain_package('elasticsearch').
+       with_ensure('0.30.3')
+    }
+    it { should contain_apt__localpackage("elasticsearch").
+       with_url("https://somehost/elasticsearch.deb")
+    }
+  end
+
+
+  describe "config file" do
+    let(:params) { { :discovery_hosts => ["s_one", "s_two", "s_three"] } }
+    it { should contain_file('/etc/elasticsearch/elasticsearch.yml').
+       with_content(/["s_one", "s_two", "s_three"]/)
+    }
+  end
+end
