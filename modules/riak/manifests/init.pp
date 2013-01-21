16ac5ca1a85071de6b863010dfb13e4e2520eee8
John Leach <john@brightbox.co.uk>
2013-01-21 13:33:19 +0000

    Riak module

---
+++ modules/riak/manifests/init.pp
@@ -0,0 +1,63 @@
+# set $cookie to a shared secret between all nodes on the cluster (this
+# is the distributed erlang cookie)
+#
+# TODO: ssl
+#
+#
+# Set $seed_host to the IP of another host in the cluster to have
+# puppet issue a !riak-admin join" command for. Leave as false to do
+# nothing. This command will be run only once for the defined seed
+# host. If you change the $seed_host option, it will re-run it on the
+# next puppet run.
+#
+# $riak_search controls the riak search feature
+#
+# $riak_control controls the riak admin panel
+#
+# If $riak_control is enabled, $riak_control_userlist should be an array
+# of : separated username and passwords, e.g: ["user1:password",
+# "user2:password"]
+
+
+class riak($cookie = "secret", $seed_host = false, $riak_search = true, $riak_control = false, $riak_control_userlist = []) {
+  apt::source { "basho":
+    source => "deb http://apt.basho.com $::lsbdistcodename main",
+    gpg_key_id => "DDF2E833"
+  }
+
+  # libssl0.9.8 is apparently needed but not a pkg dependency
+  package { "libssl0.9.8":
+    ensure => installed
+  }
+  package { "riak":
+    ensure => installed,
+    require => Package["libssl0.9.8"]
+  }
+
+  file { "/etc/riak/vm.args":
+    content => template("riak/vm.args.erb"),
+    require => Package[riak],
+    notify => Service[riak]
+  }
+
+  file { "/etc/riak/app.config":
+    content => template("riak/app.config.erb"),
+    require => Package[riak],
+    notify => Service[riak]
+  }
+
+  service { "riak":
+    ensure => running,
+    require => File["/etc/riak/app.config", "/etc/riak/vm.args"]
+  }
+
+  if $seed_host {
+    exec { "riak-seed-host":
+      path => "/usr/sbin:/usr/bin",
+      command => "riak-admin cluster join riak@$seed_host && riak-admin cluster commit && touch /var/lib/riak/joined-seed-host-${seed_host}",
+      creates => "/var/lib/riak/joined-seed-host-${seed_host}",
+      require => Service[riak]
+    }
+  }
+
+}
---
+++ modules/riak/spec/classes/riak_spec.rb
@@ -0,0 +1,52 @@
+require File.join(File.dirname(__FILE__), '../spec_helper')
+
+describe 'riak', :type => :class do
+
+  describe "default" do
+    let(:facts) { { :lsbdistcodename => 'precise', :ipaddress => '10.11.12.13' } }
+    it { should contain_package("riak").
+      with_ensure("installed")
+    }
+    it { should contain_file("/etc/riak/vm.args").
+      with_content(/-name riak@10.11.12.13/)
+    }
+    it { should contain_file("/etc/riak/app.config").
+      with_content(/{riak_search, \[{enabled, true}\]/).
+      with_content(/{riak_control, \[{enabled, false/).
+      with_content(/{userlist, \[\]}/)
+    }
+  end
+
+  describe "with cookie" do
+    let(:params) { { :cookie => "thisistheriakcookie" } }
+    let(:facts) { { :lsbdistcodename => 'precise', :ipaddress => '10.11.12.13' } }
+    it { should contain_file("/etc/riak/vm.args").
+      with_content(/-setcookie thisistheriakcookie/)
+    }
+  end
+
+  describe "with riak_search disabled" do
+    let(:params) { { :riak_search => false } }
+    let(:facts) { { :lsbdistcodename => 'precise', :ipaddress => '10.11.12.13' } }
+    it { should contain_file("/etc/riak/app.config").
+      with_content(/{riak_search, \[{enabled, false}\]/)
+    }
+  end
+
+  describe "with seed_host" do
+    let(:params) { { :seed_host => "14.15.16.17" } }
+    let(:facts) { { :lsbdistcodename => 'precise', :ipaddress => '10.11.12.13' } }
+    it { should contain_exec("riak-seed-host").
+      with_command(/join riak@14.15.16.17/)
+    }
+  end
+  describe "with riak_control" do
+    let(:params) { { :riak_control => true, :riak_control_userlist => ["john:secret", "bob:hidden"] } }
+    let(:facts) { { :lsbdistcodename => 'precise', :ipaddress => '10.11.12.13' } }
+    it { should contain_file("/etc/riak/app.config").
+      with_content(/{riak_control, \[{enabled, true/).
+      with_content(/{userlist, \[{"john", "secret"},{"bob", "hidden"}\]}/)
+    }
+  end
+end
+
