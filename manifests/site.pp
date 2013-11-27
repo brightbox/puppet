ad7c0220f406382fa3e142f0924592c5abce1832
Neil Wilson <neil@aldur.co.uk>
2013-11-27 16:59:41 +0000

    Use new class declarations

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,9 @@
 node default {
-  apt {refreshonly => false}
-  include apt_cacher_ng
+  class { "apt":
+    refreshonly => false
+  }
+  class { "apt_cacher_ng":
+  }
 
   package { "language-pack-en":
     ensure => installed
