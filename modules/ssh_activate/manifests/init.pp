70afbaa2006421df09758c39e3edd6b2d3ffff1a
Neil Wilson <neil@aldur.co.uk>
2013-11-29 15:44:50 +0000

    ssh_active module

---
+++ modules/ssh_activate/manifests/init.pp
@@ -0,0 +1,68 @@
+class ssh_activate {
+  
+  user { 'ssh_user':
+    name => 'ssh',
+    ensure => 'present',
+    comment => 'SSH activation facility',
+    gid => 'ubuntu',
+    home => '/home/ssh',
+    password => '',
+    managehome => false,
+    require => Group['ubuntu']
+  }
+
+  file { 'ssh_sudoers':
+    path 	=> '/etc/sudoers.d/90-ssh-activate',
+    owner	=> 'root', 
+    group	=> 'root',
+    mode	=> '0440',
+    content	=> template('ssh_activate/90-ssh-activate')
+  }
+
+  file { 'ssh_hushlogin':
+    path	=> '/home/ssh/.hushlogin',
+    ensure	=> present,
+    owner	=> 'ssh',
+    group 	=> 'ubuntu',
+    mode	=> '0644'
+    require	=> File['ssh_bin_dir']
+  }
+
+  file { 'ssh_profile':
+    path	=> '/home/ssh/.profile',
+    content	=> template('ssh_activate/profile')
+    ensure	=> present,
+    owner	=> 'ssh',
+    group 	=> 'ubuntu',
+    mode	=> '0644'
+    require	=> File['ssh_bin_dir']
+  }
+
+  file { 'ssh_bin_dir':
+    path	=> '/home/ssh/bin',
+    recurse	=> true,
+    ensure	=> directory,
+    owner	=> 'ssh',
+    group 	=> 'ubuntu',
+    mode	=> '0755'
+    require	=> User['ssh_user']
+  }
+
+  file { 'time_activate':
+    path	=> '/home/ssh/bin/time_activate_ssh'
+    content	=> template('ssh_activate/time_activate_ssh')
+    owner	=> 'ssh',
+    group 	=> 'ubuntu',
+    mode	=> '0755'
+    require	=> File['ssh_bin_dir']
+  }
+
+  file { 'turn_off_ssh':
+    path	=> '/etc/ssh/sshd_not_to_be_run',
+    ensure	=> present,
+    owner	=> 'root'
+    group	=> 'root'
+    mode	=> '0644'
+  }
+  
+}
