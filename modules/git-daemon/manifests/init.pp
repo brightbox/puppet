c7344e856e2aee245dd24ccb3e7563103f24bd8c
Neil Wilson <neil@aldur.co.uk>
2013-12-30 17:44:07 +0000

    Force git-daemon to work from /var/lib/git

--- modules/git-daemon/manifests/init.pp
+++ modules/git-daemon/manifests/init.pp
@@ -1,5 +1,8 @@
 class git-daemon {
 
+  $base_path='/var/lib'
+  $git_daemon_directory="${base_path}/git"
+
   package { "git-daemon":
     name => "git-daemon-sysvinit",
     ensure => installed
@@ -7,8 +10,7 @@ class git-daemon {
 
   service { "git-daemon":
     name => "git-daemon",
-    require => Package["git-daemon"],
-    subscribe => Augeas["git-daemon-default"],
+    subscribe => Augeas["git-daemon-default"], File["git-dir"]
     ensure => running,
     enable => true
   }
@@ -19,16 +21,18 @@ class git-daemon {
     lens => 'Shellvars.lns',
     changes => [
       "set GIT_DAEMON_ENABLE true",
-      "set GIT_DAEMON_OPTIONS --export-all"
+      "set GIT_DAEMON_OPTIONS --export-all",
+      "set GIT_DAEMON_BASE_PATH ${base_path}",
+      "set GIT_DAEMON_DIRECTORRY ${git_daemon_directory}"
     ]
   }
 
   file { "git-dir":
-    path => '/var/cache/git',
+    path => $git_daemon_directory,
+    ensure => directory,
     group => sudo,
     owner => root,
-    mode => '2775',
-    require => Package['git-daemon']
+    mode => '2775'
   }
 
 }
