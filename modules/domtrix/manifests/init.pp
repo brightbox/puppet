43400b07aea896593abd2ccbf39b47ade4da9153
Neil Wilson <neil@aldur.co.uk>
2014-06-13 09:13:39 +0100

    Add dummy cachedir entries to domtrix config

--- modules/domtrix/manifests/init.pp
+++ modules/domtrix/manifests/init.pp
@@ -6,7 +6,11 @@ class domtrix (
   $domtrixtype = hiera('domtrix::service'),
   $ftplogin = hiera('domtrix::ftplogin', undef),
   $ftppassword = hiera('domtrix::ftppassword', undef),
-  $notify_service = hiera('domtrix::notify_service', 'yes')
+  $notify_service = hiera('domtrix::notify_service', 'yes'),
+  $cache_dir = hiera('domtrix::cache_dir', '/tmp'),
+  $snapshot_cache_dir = hiera('domtrix::snapshot_cache_dir', '/tmp'),
+  $cache_max_blocks = hiera('domtrix::cache_max_blocks', 4096),
+  $upload_segment_size = hiera('domtrix::upload_segment_size', 104857600)
 )
 {
   
