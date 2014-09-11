fd52983b0a662a8ee4d452760b35e79c23ddbca7
John Leach <john@brightbox.co.uk>
2014-09-11 10:33:51 +0100

    Update ruby module

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -13,7 +13,7 @@ class ruby::common {
   package { "git-core":
     ensure => installed
   }
-  package { ["ruby", "ruby-dev", "rake"]:
+  package { ['ruby', 'ruby-dev', 'rake', 'make']:
     ensure => installed
   }
   package { ["bundler"]:
@@ -32,9 +32,9 @@ class ruby::rails {
 # setup ruby1.8 packages and install common dependencies and gems,
 # including bundler.
 #
-# If $system_default is true, then ruby1.8 will be the system-wide default
+# If $default is true, then ruby1.8 will be the system-wide default
 # version of ruby
-class ruby::ruby18($system_default = true) {
+class ruby::ruby18($default = true) {
   include ruby::common
   package { ["ruby1.8", "ruby1.8-dev"]:
     ensure => installed
@@ -56,13 +56,14 @@ class ruby::ruby18($system_default = true) {
   }
 
   exec { "bundle1.8-alternatives":
-    command => "/usr/sbin/update-alternatives --install /usr/bin/bundle bundle /usr/local/bin/bundle1.8 1",
+    path    => ['/usr/sbin', '/usr/bin'],
+    command => "update-alternatives --install /usr/bin/bundle bundle /usr/local/bin/bundle1.8 1",
     require => Exec["ruby1.8-install-bundler"]
   }
   
-  if $system_default {
+  if $default {
     exec { "update-alternatives-ruby1.8":
-      path => "/usr/sbin",
+      path    => ['/usr/sbin', '/usr/bin'],
       command => "update-alternatives --set ruby /usr/bin/ruby1.8 && update-alternatives --set gem /usr/bin/gem1.8 && update-alternatives --set bundle /usr/local/bin/bundle1.8",
       require => [Package["ruby"], Package["ruby1.8"], Package["rubygems"], Exec["ruby1.8-install-bundler"]]
     }
@@ -73,10 +74,10 @@ class ruby::ruby18($system_default = true) {
 # setup ruby1.9 packages and install common dependencies and gems,
 # including bundler.
 #
-# If $system_default is true, then ruby1.9 will be the system-wide default
+# If $default is true, then ruby1.9 will be the system-wide default
 # version of ruby
 
-class ruby::ruby19($system_default = true)  {
+class ruby::ruby19($default = true)  {
   include ruby::common
   package { ["ruby1.9.1", "ruby1.9.1-dev"]:
     ensure => installed
@@ -94,15 +95,61 @@ class ruby::ruby19($system_default = true)  {
   }
 
   exec { "bundle1.9-alternatives":
-    command => "/usr/sbin/update-alternatives --install /usr/bin/bundle bundle /usr/local/bin/bundle1.9.1 2",
+    path    => ['/usr/sbin', '/usr/bin'],
+    command => "update-alternatives --install /usr/bin/bundle bundle /usr/local/bin/bundle1.9.1 2",
     require => Exec["ruby1.9-install-bundler"]
   }
   
-  if $system_default {
+  if $default {
     exec { "update-alternatives-ruby1.9.1":
-      path => "/usr/sbin",
+      path    => ['/usr/sbin', '/usr/bin'],
       command => "update-alternatives --set ruby /usr/bin/ruby1.9.1 && update-alternatives --set gem /usr/bin/gem1.9.1 && update-alternatives --set bundle /usr/local/bin/bundle1.9.1",
       require => [Package["ruby"], Package["ruby1.9.1"], Exec["ruby1.9-install-bundler"]]
+    }
+  }
+
+}
+
+# setup ruby2.1 packages and install common dependencies and gems,
+# including bundler.
+#
+# If $default is true, then ruby2.1 will be the system-wide default
+# version of ruby
+
+class ruby::ruby21($default = true)  {
+  file { "/usr/local/bin/bundle":
+    ensure => "/usr/bin/bundle"
+  }
+  package { "git-core":
+    ensure => installed
+  }
+  package { ["ruby", "ruby-dev", "rake"]:
+    ensure => installed
+  }
+
+  package { ["ruby2.1", "ruby2.1-dev"]:
+    ensure => installed
+  }
+  Package["ruby2.1"] -> Package["ruby"]
+  Package["ruby2.1-dev"] -> Package["ruby-dev"]
+
+  exec { "ruby2.1-install-bundler":
+    command => "/usr/bin/gem2.1 install -f --no-rdoc --no-ri --format-executable --bindir /usr/local/bin  bundler",
+    creates => "/usr/local/bin/bundle2.1",
+    require => Package["ruby2.1"]
+  }
+
+  exec { "bundle2.1-alternatives":
+    path    => ['/usr/sbin', '/usr/bin'],
+    command => "update-alternatives --install /usr/bin/bundle bundle /usr/local/bin/bundle2.1 2",
+    require => Exec["ruby2.1-install-bundler"]
+  }
+  
+  if $default {
+    exec { "update-alternatives-ruby2.1":
+      path    => ['/usr/sbin', '/usr/bin'],
+      command => "update-alternatives --set ruby /usr/bin/ruby2.1 && update-alternatives --set gem /usr/bin/gem2.1 && update-alternatives --set bundle /usr/local/bin/bundle2.1",
+      require => [Package["ruby"], Package["ruby2.1"], Exec["ruby2.1-install-bundler"]]
     }
   }
 
