2a03db2e3542d60cc4e38e716a48e241ee3f0e27
Neil Wilson <neil@aldur.co.uk>
2014-09-12 14:30:11 +0100

    Add Git Daemon module

---
+++ modules/git-daemon/manifests/init.pp
@@ -0,0 +1,41 @@
+class git-daemon {
+
+  $base_path='/var/lib'
+  $git_daemon_directory="${base_path}/git"
+
+  package { "git-daemon":
+    name => "git-daemon-sysvinit",
+    ensure => installed
+  }
+
+  service { "git-daemon":
+    name => "git-daemon",
+    subscribe => [
+      Augeas["git-daemon-default"],
+      File["git-dir"]
+    ],
+    ensure => running,
+    enable => true
+  }
+
+  augeas { "git-daemon-default":
+    incl => '/etc/default/git-daemon',
+    require => Package["git-daemon"],
+    lens => 'Shellvars.lns',
+    changes => [
+      "set GIT_DAEMON_ENABLE true",
+      "set GIT_DAEMON_OPTIONS --export-all",
+      "set GIT_DAEMON_BASE_PATH ${base_path}",
+      "set GIT_DAEMON_DIRECTORY ${git_daemon_directory}"
+    ]
+  }
+
+  file { "git-dir":
+    path => $git_daemon_directory,
+    ensure => directory,
+    group => sudo,
+    owner => root,
+    mode => '2775'
+  }
+
+}
