35e54ace3577e7a1ca8e2ff59aea356ab62fb084
Neil Wilson <neil@aldur.co.uk>
2015-04-23 15:54:06 +0100

    Reduce buffer pool to 60%.

--- modules/percona/manifests/params.pp
+++ modules/percona/manifests/params.pp
@@ -1,5 +1,5 @@
 class percona::params {
-  $innodb_buffer_pool_size = ($::memorysizeinbytes / 1024 / 1024) * 0.70
+  $innodb_buffer_pool_size = ($::memorysizeinbytes / 1024 / 1024) * 0.60
   $key_buffer_size = ($::memorysizeinbytes / 1024 / 1024) * 0.05
   $data_dir = "/var/lib/mysql"
 }
